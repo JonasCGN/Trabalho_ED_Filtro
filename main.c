f9b0ff569bd5ef4e7dcddf3a29fd9121f4bc0be6
JonasCGN <jonasbo66@gmail.com>
2024-06-13 14:13:27 -0300

    Inserindo funçoes no gtk, e as exibindo-as

--- main.c
+++ main.c
@@ -10,27 +10,31 @@ int main(int argc, char **argv){
     HistoricoRGB *histoRGB = criaListaRGB();
     
     FILE *arq;
+    arq = fopen("./utils/input_image_example_Gray.txt","r");
+    ImageRGB *imagegray = create_image_gray(arq);
+
+    histoGray = addFinalDuplamenteCircularGray(histoGray,imagegray);
+
     arq = fopen("./utils/input_image_example_RGB.txt","r");
     ImageRGB *imagergb = create_image_rgb(arq);
 
     histoRGB = addFinalDuplamenteCircularRGB(histoRGB,imagergb);
 
-    Appdata appdata = {histoRGB,histoGray};
+    Appdata *app_data = {histoRGB,histoGray,NULL,NULL};
 
     GtkApplication *app;
     int status;
 
     app = gtk_application_new("com.swj.manipulationimage", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate), &appdata);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), &app_data);
 
     // Run the application
     status = g_application_run(G_APPLICATION(app), argc, argv);
 
     g_object_unref(app);
-
-    // free(imagergb->pixels);
-    liberaListaRGB(histoRGB);
-    liberaListaGray(histoGray);
+    g_free(app_data->historicorgb);
+    g_free(app_data->historicogray);
+    // g_free(app_data);
 
     return status;
 }
